c790d59f1ca7f656f8aa17ddf3c1c18ad6e2ea09
Engin Polat <enpolat@microsoft.com>
2025-04-24 13:43:51 -0700

    formatting document

--- VSCodeExtension/extension.ts
+++ VSCodeExtension/extension.ts
@@ -1,10 +1,6 @@
 import * as path from 'path';
 import * as cp from 'child_process';
-import {
-  LanguageClient,
-  LanguageClientOptions,
-  ServerOptions,
-} from 'vscode-languageclient/node';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node';
 import * as vscode from 'vscode';
 
 let client: LanguageClient;
