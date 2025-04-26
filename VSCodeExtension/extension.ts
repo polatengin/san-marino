b3cc326e91e50785e083c85b59839b20274adc1f
Engin Polat <enpolat@microsoft.com>
2025-04-26 15:53:44 -0700

    fix: remove unused imports from extension.ts

--- VSCodeExtension/extension.ts
+++ VSCodeExtension/extension.ts
@@ -1,5 +1,3 @@
-import * as path from 'path';
-import * as cp from 'child_process';
 import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node';
 import * as vscode from 'vscode';
 
